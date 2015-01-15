e30c87eaa67d292dc32474adba086f189870d077
Conglin Huang <conglin.huang.134@gmail.com>
2015-01-15 17:39:44 -0600

    added user manage panel

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -4,16 +4,19 @@ angular.module('itemManagementApp')
   .controller('SettingsCtrl', function ($scope, User, Auth) {
     $scope.errors = {};
 
+  // Auth, returns a function
+  $scope.isLoggedIn = Auth.isLoggedIn;
+
     $scope.changePassword = function(form) {
       $scope.submitted = true;
       if(form.$valid) {
         Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
         .then( function() {
-          $scope.message = 'Password successfully changed.';
+          $scope.message = '密码已修改';
         })
         .catch( function() {
           form.password.$setValidity('mongoose', false);
-          $scope.errors.other = 'Incorrect password';
+          $scope.errors.other = '密码错误';
           $scope.message = '';
         });
       }
